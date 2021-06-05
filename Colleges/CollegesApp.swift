e4615ea5ad0490934b6ab10c48d2cb12ab9671ac
Sachin <kashyap7024@gmail.com>
2021-06-05 09:42:30 +0530

    Initial Commit

---
+++ Colleges/CollegesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollegesApp.swift
+//  Colleges
+//
+//  Created by Sachin Kashyap on 05/06/21.
+//
+
+import SwiftUI
+
+@main
+struct CollegesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Colleges/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Colleges
+//
+//  Created by Sachin Kashyap on 05/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
